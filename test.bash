c48aa2a048e6e824176842ed1f5209434a863f10
Tsukagoshi Hayato <j3349qog@sit.ac.jp>
2025-11-26 14:45:53 +0900

    異常値対応

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Hayato Tsukagoshi
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -13,5 +13,14 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "$out" =  "" ] || ng ${LINENO}
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "$out" =  "" ] || ng ${LINENO}
+
 [ "$res" = 0 ]  && echo OK
 exit $res
