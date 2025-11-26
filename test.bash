aea728d519c5521af9e1969cccff9591263c90cf
Tsukagoshi Hayato <j3349qog@sit.ac.jp>
2025-11-26 14:55:25 +0900

    異常値対応

--- test.bash
+++ test.bash
@@ -3,10 +3,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-
 	echo NG at Line $1
 	res=1
-
 }
 
 res=0
@@ -16,11 +14,11 @@ out=$(seq 5 | ./plus)
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
-[ "$out" =  "" ] || ng ${LINENO}
+[ "{$out}" =  "" ] || ng ${LINENO}
 
 out=$(echo  | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
-[ "$out" =  "" ] || ng ${LINENO}
+[ "{$out}" =  "" ] || ng ${LINENO}
 
 [ "$res" = 0 ]  && echo OK
 exit $res
