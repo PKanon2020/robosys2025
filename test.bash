647ee884c17d515968899e84aec3f97ef937df1d
Tsukagoshi Hayato <j3349qog@sit.ac.jp>
2025-12-03 15:00:27 +0900

    fix bash

--- test.bash
+++ test.bash
@@ -12,13 +12,15 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-out=$(echo あ | ./plus)
+out=$(echo あ| ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
-[ "{$out}" =  "" ] || ng ${LINENO}
+[ "${out}" =  "" ] || ng ${LINENO}
 
 out=$(echo  | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
-[ "{$out}" =  "" ] || ng ${LINENO}
+[ "${out}" =  "" ] || ng ${LINENO}
 
 [ "$res" = 0 ]  && echo OK
+
+echo $res
 exit $res
